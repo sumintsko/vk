87356c58df9b0029585f24346e0a4adfde183c92
sumintsko <sumintsko@github.com>
2023-11-19 22:34:04 +0300

    lection 4, practice 1

---
+++ lection_4.py
@@ -0,0 +1,133 @@
+# #1
+
+
+# a = -1
+# b = 0
+
+# #Информирую сообщением о запуске программы
+# print("Start of program")
+
+# if a > 0:
+#     print("a > 0")
+# elif a > -5:
+#     print("-5 < a <= 0")
+# else:
+#     print(a <= 5)
+# print("End of program")
+
+
+#2
+#Логические конструкции
+
+
+# a = 4
+# b = 0
+# flag = True
+# flag2 = False
+
+# print(flag)
+# c = a > b
+# print(c)
+# print(a == 4)
+# print(a !=b)
+# print(a < b)
+# print(flag == flag2)
+
+
+#3
+# not, and
+
+
+# a = 4
+# b = 0
+# flag = True
+# flag2 = False
+
+
+# print(not flag)
+# print(not a < b)
+# print(a > b and a < 2)
+
+
+#4
+# or
+
+
+# a = 4
+# b = 0
+# flag = True
+# flag2 = False
+
+# print(a != b or a < b)
+# print(a < b or ((not a == b) and a > 0))
+
+
+#5
+# Convenient conditions
+
+
+# a = 4
+# b = 0
+# flag = True
+# flag2 = False
+
+
+# if a >= 0 and a < 20:
+#     print("0 <= a < 20")
+# # Можно записать так:
+# if 0 <= a < 20:
+#     print("0 <= a < 20")
+
+
+# if a > b:
+#     c = a
+# else:
+#     c = b
+# # Можно записать так:
+# c = a if a > b else b # Тернарный оператор
+
+
+#6
+# Циклы
+
+
+#for i in range(1, 10, 1):   # Аргументы (начало цикла, конец цикла, шаг цикла). i - временная переменная.
+# for i in range(10):
+# for i in "abc123":
+
+# for i in range(10):
+#     if i == 4:
+#         continue
+#     print(i)
+
+# for i in range(10):
+#     if i == 4:
+#         break
+
+#     print(i)
+# print("end")
+
+# a = 0
+# while a < 10:
+#     print(a)
+#     a += 1
+#     if a > 4:
+#         break
+
+# data = input()
+# while data:
+#     print("Entered:", data)
+#     data = input()
+# Можно записать так
+# while data := input():
+#     print("Entered:", data)
+
+
+
+
+
+
+
+
+
+
---
+++ m1l4p1.py
@@ -0,0 +1,11 @@
+#TODO Выполнить проверку диапозона чисел в границе промежутка
+
+
+data_left = int(input())
+data_right = int(input())
+while data_cycle := int(input()):
+    print("Entered:", data_cycle)
+    for data_cycle in range(data_left, data_right):
+        print("You cool baby")
+
+
