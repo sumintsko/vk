756520ce8d2a6df84f05db95ded730b7868c149a
sumintsko <sumintsko@github.com>
2023-11-22 21:43:29 +0300

    mod 4 l4p1 solution ok

--- m1l4p1.py
+++ m1l4p1.py
@@ -20,19 +20,44 @@
 #         data_cycle = False
 
 
-#mod 3
-data_left = int(input())
-data_right = int(input())
-while True:
-    data_input = int(input())
-    if data_input == '':
-        break
-    elif data_left <= data_input <= data_right:
-        data_input = True
-        # print("Число входит в промежуток")
-    else:
-        data_input = False
-        # print("Число за границами промежутка")
+#mod 3 bad
+# data_left = input()
+# data_right = input()
+# data = True
+# while True:
+#     data_input = input()
+#     if data_input == '':
+#         break
+#     elif int(data_left) <= int(data) <= int(data_right):
+#         data = True
+#     else:
+#         data = False
+# print(data)
+
+#mod 4 ok
+# data_left = int(input())
+# data_right = int(input())
+# result = True
+# while True:
+#     data_input = input()
+#     if data_input == '':
+#         break
+#     elif int(data_input) > data_right or int(data_input) < data_left:
+#         result = False
+# print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 
 
