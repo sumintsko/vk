7f53a2a3d94cf2a9c638dcf2d2e9bcb7593e94cb
sumintsko <sumintsko@github.com>
2023-11-23 00:58:45 +0300

    l4p2 solution ok

---
+++ m1l4p2.py
@@ -0,0 +1,12 @@
+flag = True
+data_input = input()
+if ("a" in data_input or "o" in data_input) and not("i" in data_input or "e" in data_input):
+    data_input = flag
+    print(data_input)
+else:
+    data_input = not flag
+    print(data_input)
+
+    
+
+
