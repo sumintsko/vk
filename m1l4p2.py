849dbfd7c3a3a0857be113f5a905bc360069cff7
sumintsko <sumintsko@github.com>
2023-11-26 20:36:50 +0300

    mod 1,2 solution ok

--- m1l4p2.py
+++ m1l4p2.py
@@ -1,12 +1,19 @@
-flag = True
-data_input = input()
-if ("a" in data_input or "o" in data_input) and not("i" in data_input or "e" in data_input):
-    data_input = flag
-    print(data_input)
-else:
-    data_input = not flag
-    print(data_input)
+#mod  1
+# flag = True
+# data_input = input()
+# if ("a" in data_input or "o" in data_input) and not("i" in data_input or "e" in data_input):
+#     data_input = flag
+#     print(data_input)
+# else:
+#     data_input = not flag
+#     print(data_input)
 
-    
+#mod 2  
+# flag = True
+# data_input = input()
+# if ("a" in data_input or "o" in data_input) and not("i" in data_input or "e" in data_input):
+#     print(True)
+# else:
+#     print(False)  
 
 
