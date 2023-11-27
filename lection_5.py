aae56a8243554c07f368dc720c0d1c7034a83a18
sumintsko <sumintsko@github.com>
2023-11-28 00:00:14 +0300

    add lection 5 notes

---
+++ lection_5.py
@@ -0,0 +1,101 @@
+# #1 Числа
+
+
+# print(1+ (2 - 4) * 3)
+# print(1 + 2.0)
+# print(4 / 2)
+
+# a = 1
+# a += 2
+# a -= 1
+# a *= 3
+# a /= 3
+
+# print(a)
+
+# print(abs(-1))
+# print(min(1, 2))
+# print(max(1, 2, 3, 4))
+# print(5 // 2)
+
+# import math
+# print(math.ceil(3.14), math.floor(3.67))
+
+# from math import log, exp
+# from math import e as exp_const
+
+# print(log(exp_const ** 10), log(exp(10)))
+
+#2 Строки
+
+
+# print("abc" + "123")
+# a = "123"
+# a += "abc"
+# print(a)
+
+# a = (
+#     "python"
+#     " is "
+#     'really'
+#     " exciting"
+# )
+# print(a)
+
+# a = "Hello World 123"
+# print(a[0], a[1], a[4])
+# print(a[5:10])
+# print(len(a))
+
+# a = "Hello World 123"
+# for symb in a[6:13:2]:
+#     print(symb)
+
+# a = "Hello World 123"
+# print("World" in a)
+# print(a.split(" "))
+
+# a = "Hello World 123"
+# print(a.replace("Hello", "Hi"))
+
+# a = "Hello World 123"
+# print(a.lower())
+# print(a.upper())
+# print(a.replace("Hello", "Hi").lower().islower())
+# print(a.find("World"))
+
+print("Hello %s %d" % ("World", 123))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
