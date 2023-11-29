f1297726f6035893a2c57c078ae1f330c5d3746c
sumintsko <sumintsko@github.com>
2023-11-30 00:27:49 +0300

    strating practice 2 for 5

---
+++ m1l5p2.py
@@ -0,0 +1,22 @@
+#mod 1
+data_input = input()
+count = len(data_input)
+print(count)
+print(data_input.lower().replace("!", " ").replace("@", " ").replace("#", " ").replace("%", " "))
+print(count)
+                        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
