4136cfe3623d02a67a2802d0e40b10861c661cd9
MCher <m.chernysh@nikasorm.ru>
2023-11-30 23:00:58 +0300

    fix for practice 2 lection 5

--- m1l5p2.py
+++ m1l5p2.py
@@ -1,13 +1,12 @@
 #mod 1
 data_input = input()
 before_count = len(data_input)
-formated_string = data_input.lower().replace("!", " ").replace("@", " ").replace("#", " ").replace("%", " ").strip()
+formated_string = data_input.lower().replace("!", "").replace("@", "").replace("#", "").replace("%", "")
 after_count = len(formated_string)
 print(before_count - after_count)
 print(formated_string)
 
                         
-
 
 
 
